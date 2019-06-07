0d4906a56024395ad5c26676ffd73040913b6012
Sergey Nikolskiy <kunneron616@gmail.com>
2019-06-07 12:49:54 +0400

    add control logic, based on: https://www.youtube.com/watch?v=NvulNx2_dKM

--- HelloWorld/src/app/components/car/car.component.ts
+++ HelloWorld/src/app/components/car/car.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CarComponent implements OnInit {
+  maxAcceleration: number;
+  minAcceleration: number;
+  fieldWidth: number;
+
   info: string;
   speed: number;
   acceleration: number;
@@ -14,35 +18,88 @@ export class CarComponent implements OnInit {
   position: HasPosition;
   cargo: string[];
   enginLable: string;
-  isFly: bolean;
+  isFly: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.maxAcceleration = 5;
+    this.minAcceleration = 1;
+    this.fieldWidth = 200;
     this.enginLable = 'START';
     this.isFly = false;
 
     this.info = 'stay on ground';
     this.speed = 0;
-    this.acceleration = 1;
+    this.acceleration = this.minAcceleration;
     this.model = "GH-700";
-    this.position = { x: 0, y: 0, z: 0 };
-    this.cargo = ["r-12", "rgh-56", "dc-21"];
-
-
+    this.position = {
+      x: this.fieldWidth / 2,
+      y: 0,
+      z: 0
+    };
+    this.cargo = [
+      "r-12 power cell",
+      "rgh-56 engine parts",
+      "dc-21 electronic components"
+    ];
   }
 
   fly() {
+    this.acceleration = this.minAcceleration;
+
     if(this.isFly) {
       this.info = 'engin stoped';
       this.enginLable = "START";
-    }
-    else {
+      this.position.y = 0;
+      this.speed = 0;
+    } else {
       this.info = 'engin started';
       this.enginLable = "STOP";
+      this.position.y = 10;
     }
 
     this.isFly = !this.isFly;
+  }
+
+  accelerate(isIncreace:boolean) {
+    if (this.isFly) {
+      if (isIncreace) {
+        this.speed = this.speed == 0
+          ? this.speed = 1
+          : this.speed;
+
+        this.acceleration = this.acceleration == this.maxAcceleration
+          ? this.acceleration
+          : this.acceleration + 1;
+      } else {
+        this.acceleration = this.acceleration == this.minAcceleration
+          ? this.acceleration
+          : this.acceleration - 1;
+      }
+    } else {
+      this.info = 'vehicle still stay on ground, start engin before fly';
+    }
+  }
+
+  horizontalMovement(isLeft:boolean) {
+    if (this.isFly) {
+      if (isLeft) {
+        this.position.x = this.position.x == 0
+          ? this.position.x
+          : this.position.x - 1;
+      } else {
+        this.position.x = this.position.x == this.fieldWidth
+          ? this.position.x
+          : this.position.x + 1;
+      }
+    } else {
+      if (isLeft) {
+        this.info = 'left door is open';
+      } else {
+        this.info = 'right door is open';
+      }
+    }
   }
 }
 
